63a23b463b56acf493735377466d22d6e7cf1382
Your Name <you@example.com>
2024-11-18 23:59:36 -0300

    config da rota de cadastro

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use App\Http\Controllers\AuthController;
 
 Route::middleware([EnsureFrontendRequestsAreStateful::class, 'throttle:api'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 });
+
+Route::post('/register', [AuthController::class, 'register']);
