f1512236c810699ed446a40d2fd1d7e78d805707
Your Name <you@example.com>
2024-11-19 21:47:16 -0300

    metodo de login e de logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,4 +27,33 @@ class AuthController extends Controller
 
         return response()->json(['message' => 'User created successfully'], 201);
     }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Successfully logged out'], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,6 @@ Route::middleware([EnsureFrontendRequestsAreStateful::class, 'throttle:api'])->g
     });
 });
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('/register', [AuthController::class, 'register']);
