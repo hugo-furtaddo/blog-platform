2133972e1e461b10cf545b7e7aa47d91d37a186c
Your Name <you@example.com>
2024-11-19 22:20:06 -0300

    add atributos de Comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,15 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'content',
+        'post_id',
+        'user_id',
+    ];
+
+    /**
+     * A comment belongs to a post.
+     */
     public function post()
     {
         return $this->belongsTo(Post::class);
     }
 
+    /**
+     * A comment belongs to a user.
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
